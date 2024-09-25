4ba59185c4599c6585bbbc7d86d35fb7ac9d8202
Obedlo93 <obedlo93@gmail.com>
2024-09-25 17:33:04 -0600

    componentes
    componentes login

---
+++ proyectoux/src/app/pages/nueva-contrasena/nueva-contrasena.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-contrasena',
+  standalone: true,
+  imports: [],
+  templateUrl: './nueva-contrasena.component.html',
+  styleUrl: './nueva-contrasena.component.css'
+})
+export class NuevaContrasenaComponent {
+
+}
