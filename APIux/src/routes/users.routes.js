6f652897f6e68ee71fbbc97cdefc66a0a6b6db8b
BrandonChacon01 <dancam00223@gmail.com>
2024-09-26 17:38:32 -0600

    Update API
    Se modifica la API

--- APIux/src/routes/users.routes.js
+++ APIux/src/routes/users.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { 
-    postDataUser,
     postNewUser
+    // postDataUser,
 } from "../controllers/users.controllers.js";
 
 const router = Router();
@@ -11,6 +11,6 @@ const router = Router();
 router.post("/user/new", postNewUser);
 
 // GET DATA USER POR ID DE USUARIO
-router.post("/data/user", authMiddleware, postDataUser);
+// router.post("/data/user", authMiddleware, postDataUser);
 
 export default router;
